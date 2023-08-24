b3b9a803374d73c421782fd70313c31b84b79b81
Amidaa123 <lebedev.d.5@mail.ru>
2023-08-24 20:33:56 +0400

    Занятие 9 успешно

--- advertisement/app_advertisement/models.py
+++ advertisement/app_advertisement/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib import admin
 from django.utils import timezone
 from django.utils.html import format_html
+from django.contrib.auth import get_user_model
+User = get_user_model()
 class Advertisement(models.Model):
     title = models.CharField(
         max_length=80,
@@ -33,6 +35,17 @@ class Advertisement(models.Model):
         verbose_name="Дата редактирования"
     )
 
+    user = models.ForeignKey(
+        User,
+        verbose_name="Пользователь",
+        on_delete=models.CASCADE
+    )
+
+    image = models.ImageField(
+        verbose_name="Изображение",
+        upload_to="advertisements/"
+    )
+
     @admin.display(description="Дата создания")
     def created_date(self):
         if self.created_at.date() == timezone.now().date():
@@ -40,16 +53,16 @@ class Advertisement(models.Model):
             return format_html(
                 '<span style="color: green; font-weight: bold;">Сегодня в {}</span>', created_time
             )
-        return self.created_at.strftime("%d.%m.%Y в %H:%M:%S")
+        return self.created_at.strftime("%d.%m.%Y | %H:%M:%S ")
 
     @admin.display(description="Дата обновления")
     def updated_date(self):
         if self.updated_at.date() == timezone.now().date():
             updated_time = self.updated_at.time().strftime("%H:%M:%S")
             return format_html(
-                '<span style="color: green; font-weight: bold;">Сегодня в {}</span>', updated_time
+                '<span style="color: blue; font-weight: bold;">Сегодня в {}</span>', updated_time
             )
-        return self.updated_at.strftime("%d.%m.%Y в %H:%M:%S")
+        return self.updated_at.strftime("%d.%m.%Y | %H:%M:%S")
     def __str__(self):
         return f"id = {self.id} title = {self.title} price = {self.price}"
 
--- advertisement/app_advertisement/views.py
+++ advertisement/app_advertisement/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
+from .models import Advertisement
 
 # Create your views here.
 def index(requests):
-    return render(requests, "index.html")
+    advertisements = Advertisement.objects.all()
+    context = {"advertisements": advertisements}
+    return render(requests, "index.html", context=context)
 
 def top_sellers(request):
     return render(request, "top-sellers.html")
