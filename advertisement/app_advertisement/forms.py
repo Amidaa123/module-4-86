28abaa26e8227e15aab032e2b5627ea16af4b9a6
Amidaa123 <lebedev.d.5@mail.ru>
2023-08-26 13:52:06 +0400

    Домашка по 10 занятию

--- advertisement/app_advertisement/forms.py
+++ advertisement/app_advertisement/forms.py
@@ -1,20 +1,40 @@
 from django import forms
+from django.forms import models
+from .models import Advertisement
+from django.core.exceptions import ValidationError
 
-class AdvertisementForm(forms.Form):
-    title = forms.CharField(
-        max_length=60,
-        widget=forms.TextInput(attrs={"class": "form-control form-control-lg"})
-    )
-    description = forms.CharField(
-        widget=forms.Textarea(attrs={"class": "form-control form-control-lg"})
-    )
-    price = forms.DecimalField(
-        widget=forms.NumberInput(attrs={"class": "form-control form-control-lg"})
-    )
-    auction = forms.BooleanField(
-        required=False,
-        widget=forms.CheckboxInput(attrs={"class": "form-check-input"})
-    )
-    image = forms.ImageField(
-        widget=forms.FileInput(attrs={"class": "form-control form-control-lg"})
-    )
+# class AdvertisementForm(forms.Form):
+#     title = forms.CharField(
+#         max_length=60,
+#         widget=forms.TextInput(attrs={"class": "form-control form-control-lg"})
+#     )
+#     description = forms.CharField(
+#         widget=forms.Textarea(attrs={"class": "form-control form-control-lg"})
+#     )
+#     price = forms.DecimalField(
+#         widget=forms.NumberInput(attrs={"class": "form-control form-control-lg"})
+#     )
+#     auction = forms.BooleanField(
+#         required=False,
+#         widget=forms.CheckboxInput(attrs={"class": "form-check-input"})
+#     )
+#     image = forms.ImageField(
+#         widget=forms.FileInput(attrs={"class": "form-control form-control-lg"})
+#     )
+class AdvertisementForm(models.ModelForm):
+    class Meta:
+        model = Advertisement
+        fields = ['title', 'description', 'price', 'auction', 'image']
+        widgets = {
+            'title': forms.TextInput(attrs={"class": "form-control form-control-lg"}),
+            'description': forms.Textarea(attrs={"class": "form-control form-control-lg"}),
+            'price': forms.NumberInput(attrs={"class": "form-control form-control-lg"}),
+            'auction': forms.CheckboxInput(attrs={"class": "form-check-input"}),
+            'image': forms.FileInput(attrs={"class": "form-control form-control-lg"})
+        }
+
+    def clean_title(self):
+        title = self.cleaned_data.get('title')
+        if title and title.sterswith('?'):
+            raise ValidationError("Ошибка")
+        return title
